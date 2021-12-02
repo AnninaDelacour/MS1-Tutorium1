d6463d76ad4e39d4baca77b4fd54d6a12b81debe
Annina Ecker <anninaecker@MacBook-Pro-8.local>
2021-12-02 16:09:29 +0100

    added powerOf.c

---
+++ powerOf.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        printf("Error!\n");
+        return EXIT_FAILURE;
+    }
+
+    int a = strtol(argv[1], NULL, 10);
+    if(a < 0)
+    {
+        printf("Bitte gib eine positive Ganzzahl ein!\n");
+        return EXIT_FAILURE;
+    }
+
+
+    bool isNotPowerOf = false;
+    int counter = 0;
+
+    while(a > 1)
+    {
+        if(a % 2 != 0)
+        {
+            isNotPowerOf = true;
+            break;
+        }
+        a = a / 2;
+        counter++;
+    }
+
+    if(!isNotPowerOf){
+        printf("%d\n", counter);
+    }
+    else{
+        printf("Keine Zweierpotenz!\n");
+    }
+
+    return EXIT_SUCCESS;
+}
